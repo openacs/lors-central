8e1b89471a3a830a6ec8cd2686506f4a70adbb57
Miguel Marin <miguelmmarin@viaro.net>
2005-05-20 18:49:28 +0000

    removing no longern necesary lines

--- sql/postgresql/lors-central-create.sql
+++ sql/postgresql/lors-central-create.sql
@@ -1,8 +1,3 @@
-ALTER TABLE ims_cp_manifest_class DROP CONSTRAINT ims_cp_manifest_class_pkey;
-ALTER TABLE ims_cp_items ADD sort_order integer;
-ALTER TABLE ims_cp_files DROP CONSTRAINT ims_cp_files_file_if_fk;
-ALTER TABLE ims_cp_files ADD CONSTRAINT ims_cp_files_file_if_fk FOREIGN KEY (file_id) REFERENCES cr_revisions(revision_id) ON DELETE CASCADE;
-
 create table ims_cp_items_map (
     man_id         int
                    constraint ims_cp_items_map_man_id_fk references ims_cp_manifests (man_id),
