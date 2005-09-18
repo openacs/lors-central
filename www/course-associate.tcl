db04280271e2cdda679c8d4bf2860eadaa58d943
Dave Bauer <dave@thedesignexperience.org>
2005-09-18 14:27:58 +0000

    Return to one-course-associations so you can see the result of your action.

--- www/course-associate.tcl
+++ www/course-associate.tcl
@@ -41,7 +41,7 @@ foreach community_id $object_id {
 
 
 if { [empty_string_p $return_url] } {
-    ad_progress_bar_end -url "course-dotlrn-assoc?item_id=$item_id"
+    set return_url [export_vars -base one-course-associations {man_id}]
 } else {
     ad_progress_bar_end -url $return_url
 }
