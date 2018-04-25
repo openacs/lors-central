74fd423133d12b09af8686f78a3d06b32f1f7988
Hector Romojaro <hector.romojaro@gmail.com>
2018-04-25 19:47:47 +0000

    Prefer '{}' to '[list]' when creating empty lists

--- tcl/lors-central-cr-procs.tcl
+++ tcl/lors-central-cr-procs.tcl
@@ -26,7 +26,7 @@ ad_proc -public lors_central::cr::add_files {
     # Get the ip
     set creation_ip [ad_conn peeraddr]
 
-    set retlist [list]
+    set retlist {}
     foreach fle $files {
 
 	regexp {[^//\\]+$} $fle filename
